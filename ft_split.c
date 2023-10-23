378027263f512f8d4a4404c284ce3fa3c37f001b
Alex Gilles <agilles@c2r2p3.42lehavre.fr>
2023-10-23 17:27:48 +0200

    aled

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: agilles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:31:30 by agilles           #+#    #+#             */
-/*   Updated: 2023/10/23 15:24:48 by agilles          ###   ########.fr       */
+/*   Updated: 2023/10/23 17:21:43 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	count_word(char const *s, char c)
 
 	i = -1;
 	count = 0;
-	while (s[i++])
+	while (s[++i])
 	{
 		if (s[i] == c && i == 0)
 		{
@@ -40,7 +40,53 @@ int	count_word(char const *s, char c)
 	return (count);
 }
 
+int	word_pos(char const *s, char c, int i)
+{
+	while (s[i] == c)
+		i++;
+	return (i);
+}
+
+int	word_len(char const *s, char c, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s[i] != '\0' && s[i] != c)
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	char	**split;
+	int	index;
+	int	i;
+	int	j;
 	
+	index = 0;
+	j = 0;
+	i = 0;
+	split = malloc(sizeof(char *) * (count_word(s, c) + 1));
+	if (!split)
+		return (NULL);
+	while (index < count_word(s, c))
+	{
+		i = word_pos(s, c, i);
+		split[index] = malloc(sizeof(char) * (word_len(s, c, i) + 1));
+		if (!split[index])
+		{
+			while (index >= 0)
+				free(split[index--]);
+			free(split);
+		}
+		while (s[i] != '\0' && s[i] != c)
+			split[index][j++] = s[i++];
+		split[index++][j] = '\0';
+		j = 0;
+	}
+	return (split);
 }
