671209923b62cbbc27f17d6dc5094c9eff86766e
Alex Gilles <agilles@c2r2p6.42lehavre.fr>
2023-10-26 16:58:49 +0200

    bonus#2

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,44 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agilles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 16:19:50 by agilles           #+#    #+#             */
-/*   Updated: 2023/10/26 16:58:12 by agilles          ###   ########.fr       */
+/*   Created: 2023/10/26 16:31:10 by agilles           #+#    #+#             */
+/*   Updated: 2023/10/26 16:31:50 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_lstsize(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
-	int	i;
-
-	i = 0;
-	while (lst)
-	{
-		i++;
-		lst = lst->next;
-	}
-	return (i);
+	
 }
-
-/*int	main(void)
-{
-	int	n;
-	t_list	*lst1;
-	t_list	*lst2;
-	t_list	*lst3;
-
-	lst1 = malloc(sizeof(t_list));
-	lst2 = malloc(sizeof(t_list));
-	lst3 = malloc(sizeof(t_list));
-
-	lst1->next = lst2;
-	lst2->next = lst3;
-
-	n = ft_lstsize(lst1);
-	printf("%d", n);
-}*/
